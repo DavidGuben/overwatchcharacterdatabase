8a5724a7db0ed65ffda568e065448dcb5baea077
David Guben <davidjg6@gmail.com>
2017-05-06 17:37:22 -0400

    added header and fixed some syntax

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ export const Layout = props => (
   <div className="app-container">
     <header>
       <Link to="/">
-        <img className="logo" src="/../../static/img/overwatch-logo-img.png" alt="Overwatch Logo" />
+        <img className="logo" src="/img/overwatch-character-database-banner.jpg" alt="Overwatch Logo" />
       </Link>
     </header>
     <div className="app-content">{props.children}</div>
